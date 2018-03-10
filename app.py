89415d12bb7c34d6517ebfcc3fe070188825f616
Jack Laxson <jackjrabbit@gmail.com>
2018-03-09 21:40:33 -0500

    current status of bot

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import tweepy
 import toml
+import arrow
+import logging
+from typing import List, Union, Dict
 
+
+logger = logging.getLogger(__name__)
 OUR_CONF = "frogborg.toml"
+
+def get_tweets(api) -> List[tweepy.Status]:
+    try:
+        public_tweets = api.home_timeline()
+        return public_tweets
+    except tweepy.error.RateLimitError:
+        logger.info("Hit rate-limit.")
+    except ConnectionError as e:
+        logger.info("ConnectionError: %s", e)
+    except tweepy.error.TweepError as e:
+        logger.info("Other TweepyError: %s", e)
+    return []
+
+def handle_tweet(tweet) -> None:
+    print(tweet)
 
 def start(settings) -> None:
     auth = tweepy.OAuthHandler(settings['twitter']['auth']['consumer_key'], settings['twitter']['auth']['consumer_secret'])
@@ -11,7 +31,7 @@ def start(settings) -> None:
     me = api.me()
     logger.info("Got API, starting twitter module...")
     while True:
-        for t in get_tweets():
+        for t in get_tweets(api):
             handle_tweet(t)
         last_look = arrow.utcnow()
         logger.debug("Sleeping for {} seconds".format(settings['twitter']['cooldown']))
